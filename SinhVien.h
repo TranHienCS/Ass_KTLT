e42e23fda4e1b666003c61be3b0fe654359ac924
TranHienCS <32125746+TranHienCS@users.noreply.github.com>
2018-06-02 23:55:51 +0700

    Update SinhVien.h

--- SinhVien.h
+++ SinhVien.h
@@ -1,9 +1,13 @@
 #pragma once
+#include"DangNhap.h"
 #include<iostream>
 #include<string>
 #include<fstream>
 #include<sstream>
 using namespace std;
+struct sinhvien {
+	string ID_sv, FullName_sv,BirthDay_sv, Address_sv;
+};
 class SinhVien {
 private:
 	string ID_sv, FullName_sv, Address_sv;
@@ -16,7 +20,7 @@ public:
 	void CourseRegistration();
 	void DropCouse();
 	void viewPersonalInformation();
-
+	void ScreenSV();
 	~SinhVien();
 };
 
