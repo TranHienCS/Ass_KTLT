538efd4b57b28e9765f5ca3b544ddb88cd910da7
TranHienCS <32125746+TranHienCS@users.noreply.github.com>
2018-06-02 23:57:54 +0700

    Update Admin.h

--- Admin.h
+++ Admin.h
@@ -1,12 +1,19 @@
 #pragma once
+#include "GiangVien.h"
+#include "DangNhap.h"
+#include "SinhVien.h"
+#include "User.h"
+#include<iomanip>
 class Admin
 {
 public:
 	Admin();
 
-	virtual void changePass();
+	
 	void Add();
 	void Delete();
+	void FindUser();
+	void ScreenAdmin();
 
 	~Admin();
 };
